5fdbb561acacf970ddf7d7595d7e77b6710acfcf
enriqueatenco99 <34581275+enriqueatenco99@users.noreply.github.com>
2017-12-15 11:43:56 -0600

    Add files via upload

---
+++ ejemplo2/main.cpp
@@ -0,0 +1,10 @@
+#include <string> 
+#include <iostream> 
+ using namespace std; 
+  
+ main() 
+ { 
+  string mensaje; 
+  mensaje =" Que tal enrique estudias sistemas computacionales"; 
+ cout << mensaje; 
+ }  
---
+++ ejemplos/main.cpp
@@ -0,0 +1,10 @@
+#include <string> 
+ #include <iostream> 
+ using namespace std; 
+   
+ main() 
+ { 
+  string mensaje; 
+  mensaje = "Hola"; 
+  cout << mensaje; 
+ }  
